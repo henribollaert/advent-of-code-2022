0e856ce6ea79943ab9fa6e12e6260155665b9dd0
hebollae <Henri.Bollaert@UGent.be>
2022-12-09 15:05:33 +0100

    day 4 refactor

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -4,20 +4,18 @@ def part1(lines):
     amount = 0
     for line in lines:
         intervals = line.split(',')
-        if (int(intervals[0].split('-')[0]) <= int(intervals[1].split('-')[0]) and
-                int(intervals[0].split('-')[1]) >= int(intervals[1].split('-')[1])) or \
-                (int(intervals[0].split('-')[0]) >= int(intervals[1].split('-')[0]) and
-                int(intervals[0].split('-')[1]) <= int(intervals[1].split('-')[1])):
-            amount += 1
+        amount += (int(intervals[0].split('-')[0]) <= int(intervals[1].split('-')[0]) and
+                   int(intervals[0].split('-')[1]) >= int(intervals[1].split('-')[1])) or \
+                  (int(intervals[0].split('-')[0]) >= int(intervals[1].split('-')[0]) and
+                   int(intervals[0].split('-')[1]) <= int(intervals[1].split('-')[1]))
     return amount
 
 def part2(lines):
     amount = 0
     for line in lines:
         intervals = line.split(',')
-        if not (int(intervals[0].split('-')[1]) < int(intervals[1].split('-')[0]) or \
-                int(intervals[0].split('-')[0]) > int(intervals[1].split('-')[1])):
-            amount += 1
+        amount += not (int(intervals[0].split('-')[1]) < int(intervals[1].split('-')[0]) or
+                       int(intervals[0].split('-')[0]) > int(intervals[1].split('-')[1]))
     return amount
 
 def main():
@@ -26,10 +24,6 @@ def main():
     f = open(os.path.join(__location__, 'input-4.txt'), 'r')
     lines = f.readlines()
     f.close()
-
-#     lines = ["24-66,23-25",
-# "3-3,2-80",
-# "14-80,14-20"]
 
     print("Score for part 1:", part1(lines))
     print("Score for part 2:", part2(lines))
