bc97b3d5d0e59718eef6b5b810afb0266daf37a1
hebollae <Henri.Bollaert@UGent.be>
2022-12-09 14:05:05 +0100

    prettier day 9

--- day-9/day-9.py
+++ day-9/day-9.py
@@ -5,14 +5,16 @@ import os
 X_DIF = {'R' : 1, 'U' : 0,  'D' : 0, 'L' : -1}
 Y_DIF = {'R' : 0, 'U' : 1,  'D' : -1, 'L' : 0}
 
-def distance(pair1, pair2):
-    """calculate chebyshev distance"""
-    return max(abs(pair1[0] - pair2[0]), abs(pair1[1] - pair2[1]))
+def sign(a):
+    return (a > 0) - (a < 0)
+
+def chebyshev_distance(x1, x2):
+    return max([abs(sum(pair)) for pair in zip(x1, [-1 * x for x in x2])])
 
 def manhattan_length(pair):
     return sum([abs(c) for c in pair])
 
-def part1(lines, verbose=False):
+def part1old(lines, verbose=False):
     head = (0,0)
     tail = (0,0)
     tail_positions = set([tail])
@@ -21,71 +23,37 @@ def part1(lines, verbose=False):
         for _ in range(int(amount)):
             prev = head
             head = tuple(map(sum, zip(head, (X_DIF[direction], Y_DIF[direction]))))
-            if distance(head, tail) > 1:
+            if chebyshev_distance(head, tail) > 1:
                 tail = prev
                 tail_positions.add(tail)
         if verbose:
             print(f"===== {direction} {amount} =====\nhead: {head}\ntail: {tail}")
     return len(tail_positions)
 
-def simulate_rope(moves, length):
+
+def simulate_rope(moves, length=10):
     """
-    length = numbered knots so H, 1, 2, ..., 9 has length 9
+    length includes H: H, 1, 2, ..., 9 has length 10
     """
-    head = (0,0)
     knots = [(0,0) for _ in range(length)]
     tail_positions = set([knots[-1]])
-    tail_list = [knots[-1]]
     for direction, amount in moves:
         for _ in range (amount):
-            prev = head
-            head = tuple(map(sum, zip(head, (X_DIF[direction], Y_DIF[direction]))))
-            # first
-            cur = knots[0]
-            if distance(head, knots[0]) > 1:
-                knots[0] = prev
-            prev_move_length = manhattan_length((knots[0][0] - cur[0], knots[0][1] - cur[1]))
-            prev = cur
-
-            # rest
+            knots[0] = tuple(map(sum, zip(knots[0], (X_DIF[direction], Y_DIF[direction]))))
             i = 1
-            while i < length and distance(knots[i - 1], knots[i]) > 1:
-                cur = knots[i]
-                if prev_move_length == 1:
-                    # classic
-                    knots[i] = prev
-                else:
-                    # longer
-                    new_x, new_y = knots[i]
-                    if knots[i-1][0] < knots[i][0]:
-                        new_x -= 1
-                    elif knots[i-1][0] > knots[i][0]:
-                        new_x += 1
-
-                    if knots[i-1][1] < knots[i][1]:
-                        new_y -= 1
-                    elif knots[i-1][1] > knots[i][1]:
-                        new_y += 1
-                    knots[i] = (new_x, new_y)
-                    # knots[i] = tuple(map(sum, zip(knots[i], prev_move)))
-                # if knots[i - 1][0] != knots[i][0] and knots[i - 1][1] != knots[i][1]:
-                #     # is this wrong?
-                #     if sum([abs(c) for c in prev_move]) > 1:
-                #         knots[i] = tuple(map(sum, zip(knots[i], prev_move)))
-                #     else:
-                #         knots[i] = prev
-                # else:
-                #     knots[i] = tuple(map(sum, zip(knots[i], (X_DIF[direction], Y_DIF[direction]))))
-                prev_move_length = manhattan_length((knots[i][0] - cur[0], knots[i][1] - cur[1]))
-                prev = cur
+            while i < length and chebyshev_distance(knots[i - 1], knots[i]) > 1:
+                knots[i] = tuple(map(sum, zip(knots[i], 
+                                              (sign(knots[i-1][0] - knots[i][0]),
+                                               sign(knots[i-1][1] - knots[i][1])))))
                 i += 1
             tail_positions.add(knots[-1])
-            tail_list.append(knots[-1])
     return len(tail_positions)
 
+def part1(lines):
+    return simulate_rope([(line.split()[0], int(line.split()[1])) for line in lines], 2)
+
 def part2(lines):
-    moves = [(line.split()[0], int(line.split()[1])) for line in lines]
-    return simulate_rope(moves, length=9)
+    return simulate_rope([(line.split()[0], int(line.split()[1])) for line in lines], 10)
 
 def main():
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
